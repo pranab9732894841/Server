33e6c5e811cbfb4afee8bc8f692274e3b8d34375
spidercode <djpronixx.official@gmail.com>
2021-06-24 16:56:16 +0530

    auth create

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/users");
+const getProduct = require("../middleware/getProduct");
+
+//singin
+router.get("/", async (req, res) => {
+  try {
+    const user = await Users.findByCredentials(
+      req.body.email,
+      req.body.pasword
+    );
+    console.log(user);
+    const token = await user.generateAuthToken();
+    res.status(202).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//singup
+router.post("/", async (req, res) => {
+  const user = new Users(req.body);
+  try {
+    const token = await user.generateAuthToken();
+    res.status(202).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//getprofile
+
+//updateprofile
+
+//Export
+module.exports = router;
