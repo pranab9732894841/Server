2de805583c18473eb2f7cb58f4392ae7e423379f
Spidewave <djpronixx.official@gmail.com>
2021-08-05 16:45:58 +0530

    move to mongo db atlass

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const mongoose = require("mongoose");
 
 //detabase conections
-mongoose.connect(process.env.LOCAL_DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect(process.env.ONLINE_DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true  });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
